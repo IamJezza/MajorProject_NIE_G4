d43512d3edff7c204b04940ee2d75d5855412530
IamJezza <124605311+IamJezza@users.noreply.github.com>
2023-04-27 16:49:12 +0530

    Add files via upload

--- extract_undectectable_faults.py
+++ extract_undectectable_faults.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
 
-my_dict = {}
 unDetectableFaults = []
 diff_data = pd.read_csv('DIFF_DATA.csv')
 for i in range(1, 9):
     for j in range(1, 29):
         my_list = np.array(diff_data['out'+str(i)+'[0; '+str(j)+']']).nonzero()
         if list(my_list[0]):
-            my_dict['out'+str(i)+'[0; '+str(j)+']'] = list(my_list[0])
+            pass
         else:
             unDetectableFaults.append('out'+str(i)+'[0; '+str(j)+']')
 
@@ -16,4 +15,5 @@ for i in range(1, 9):
 useful_diff_data = diff_data.drop(unDetectableFaults, axis=1)
 unuseful_diff_data = pd.DataFrame(unDetectableFaults)
 
-unuseful_diff_data.to_csv('UNUSEFUL_DIFF_DATA.csv', index=False, header=False)
+useful_diff_data.to_csv('USEFUL_DIFF_DATA.csv', index=False)
+unuseful_diff_data.to_csv('UNUSEFUL_CASES.csv', index=False, header=False)
